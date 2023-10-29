560dca439f16041189ee190ba39148eb802eec86
Yukimai <yukimaikoriya@vip.qq.com>
2023-10-29 10:12:48 -0700

    Add cam calibration

--- april_detection/src/april_detection_node.cpp
+++ april_detection/src/april_detection_node.cpp
@@ -42,18 +42,18 @@ ros::Publisher apriltag_pub;
 ros::Subscriber image_sub;
 AprilDetection det;
 
-// TODO: Replace these parameters using your calibration results
+// Done: Replace these parameters using your calibration results
 double distortion_coeff[] = 
-	{0.008549, -0.016273, -0.002954, -0.003708, 0.000000};
-double intrinsics[] = {683.558755,    0.     ,  961.607445,
-                       0.     ,  680.809134,  547.701668,
-                       0.     ,    0.     ,    1.};
+	{0.026766, -0.020853, 0.001030, -0.000917, 0.000000};
+double intrinsics[] = {694.82753,    0.     ,  957.10001,
+            0.     ,  693.47137,  527.86315,
+            0.     ,    0.     ,    1. };
 
 const cv::Mat d(cv::Size(1, 5), CV_64FC1, distortion_coeff);
 const cv::Mat K(cv::Size(3, 3), CV_64FC1, intrinsics);
-// TODO: Set tagSize for pose estimation, assuming same tag size.
+// Done: Set tagSize for pose estimation, assuming same tag size.
 // details from: https://github.com/AprilRobotics/apriltag/wiki/AprilTag-User-Guide#pose-estimation
-const double tagSize = 0.162; // in meters
+const double tagSize = 0.111; // in meters
 
 cv::Mat rectify(const cv::Mat image){
   cv::Mat image_rect = image.clone();
